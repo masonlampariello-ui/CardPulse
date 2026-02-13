39f00c2f71c478687ab0afd426ead147370c4218
masonlampariello-ui <masonlampariello@gmail.com>
2026-02-12 19:56:50 -0500

    Add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.stripe.com',
+      },
+    ],
+  },
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+}
+
+module.exports = nextConfig
